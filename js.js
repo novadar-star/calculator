fe39c67cea267131d744f05ccd4f1ed34f16cb76
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-24 17:38:18 +0800

    Fetch ids and assigned to a variable

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
 //append what value of button to the screen
+
+btnOne = document.getElementById('one');
+btnTwo = document.getElementById('two');
+btnThree = document.getElementById('three');
+btnFour = document.getElementById('four');
+
+btnFive = document.getElementById('five');
+btnSix = document.getElementById('six');
+btnSeven = document.getElementById('seven');
+btnEight = document.getElementById('eight');
+
+
+btnNine= document.getElementById('nine');
+btnAdd = document.getElementById('add');
+btnSub = document.getElementById('subtract');
+btnTimes = document.getElementById('multiply');
+
+btnDiv = document.getElementById('divide');
+btnDec = document.getElementById('decimal');
+btnEq = document.getElementById('equal');

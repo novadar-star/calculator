4b69b0216970ea3fbd978f4b6608d733a79b389a
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-25 19:09:46 +0800

    Added if conditionals for storing input numbers

--- js.js
+++ js.js
@@ -1,25 +1,41 @@
 
-const btns = document.querySelectorAll("button")
-const equals = document.querySelector("#equal")
+const numBtn = document.querySelectorAll(".num")
 const operBtn = document.querySelectorAll(".operator")
+const equals = document.querySelector("#equal")
+
 let clickBtn = 0;
 
-btns.forEach(btn=> {
-    btn.addEventListener("click", (e)=>{
-        clickBtn = e.target.value; //dont output if value
-
-    document.querySelector("#textfield").value += clickBtn;
-    })
-})
-
-//document.querySelector("#equal").addEventListener("click", ()=> param)
-//need to figure out once operation clicked you will have a new set of screen where u will apply the second parameter
-//STEP 6
-//three variables
 let num1 = "";
 let num2 = "";
 let operator = "";
 let result = "";
+
+//outputs the value on the screen
+numBtn.forEach(btn=> {
+    btn.addEventListener("click", (e)=>{
+        clickBtn = e.target.value; //dont output if value
+    document.querySelector("#textfield").value += clickBtn;
+    })
+})
+
+
+numBtn.forEach(num=> {
+    num.addEventListener("click", (e)=>{
+        if(operator === ""){ //will read first number if operator is empty
+            num1 += e.target.value;
+            alert(num1)
+        }
+        else{ //if num is inputted will read the sec number
+            num1 += e.target.value;
+            alert(num2)
+        }
+    } )
+})
+//document.querySelector("#equal").addEventListener("click", ()=> param)
+//need to figure out once operation clicked you will have a new set of screen where u will apply the second parameter
+//STEP 6
+//three variables
+
 
 function operate(num1, num2, operator){
     switch(operator){

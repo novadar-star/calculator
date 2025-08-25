b554f22acbb31c3b9fa5c078ff272e4bb49d26fd
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-25 23:02:23 +0800

    It doesnt accumulate now, but weirdly outputs wrong answer LMAOOO

--- js.js
+++ js.js
@@ -2,36 +2,25 @@
 const numBtn = document.querySelectorAll(".num")
 const operBtn = document.querySelectorAll(".operator")
 
-let clickBtn = 0;
 
 let num1 = "";
 let num2 = "";
 let operator = "";
 let result = "";
 
-//outputs the value on the screen
-/*
-numBtn.forEach(btn=> {
-    btn.addEventListener("click", (e)=>{
-        clickBtn = e.target.value; //dont output if value
-    document.querySelector("#textfield").value += clickBtn;
-    })
-})
-    */
-    
-
+//num1, num2 is string while res is number wtf?
 //storing the clicked btn
 numBtn.forEach(num=> {
     num.addEventListener("click", (e)=>{
         if(operator === ""){ //will read first number if operator is empty
-            num1 += e.target.value;
-            document.querySelector("#textfield").value += num1;
-            //alert(num1)
+            num1 = e.target.value;
+            document.querySelector("#textfield").value = num1;
+            //alert(typeof num1)
         }
         else{ //if num is inputted will read the sec number
-            num2 += e.target.value;
-            document.querySelector("#textfield").value += num2;
-           // alert(num2)
+            num2 = e.target.value;
+            document.querySelector("#textfield").value = num2;
+           //alert(typeof num2)
         }
     })
 })
@@ -41,11 +30,10 @@ operBtn.forEach(oper=> {
 
         if(e.target.value !== "="){ //as long as equal btn is click will print to screen
             operator = e.target.value;
-            document.querySelector("#textfield").value += operator;
+            document.querySelector("#textfield").value = operator;
         }
         else{ //if equal btn clicked; numerical operations
-    
-              textfield.value = ""; //clears the input field 
+            textfield.value = ""; //clears the input field 
          switch(operator){
             case "+":
                 result = add(num1,num2)
@@ -60,41 +48,17 @@ operBtn.forEach(oper=> {
             result = divide(num1,num2)
                 break;
             }
-            document.querySelector("#textfield").value += result; 
+            alert(result)
+            document.querySelector("#textfield").value = result; 
+            
     }
 })
 })
-//document.querySelector("#equal").addEventListener("click", ()=> param)
-//need to figure out once operation clicked you will have a new set of screen where u will apply the second parameter
-//STEP 6
-//three variables
 
-/*
-function operate(num1, num2, operator){
-    switch(operator){
-        case "+":
-            result = add(num1,num2)
-        case "-":
-            result =subtract(num1,num2)
-        case "x":
-            result = multiply(num1,num2)
-        case "÷":
-           result = divide(num1,num2)
-    }
-     document.querySelector("#textfield").value += result;
-}
-
-operBtn.forEach(operate => {
-    operate.addEventListener("click", function(){
-
-    })
-})
-    */
 
 function add(a,b){
     return a + b
 }
-
 function subtract(a,b){
     return a - b
 }
@@ -103,7 +67,8 @@ function multiply(a,b){
 }
 function divide(a,b){
     if(b === 0){
-        return NaN;
+        return NaN
+        return document.querySelector("#textfield").innerHTML = "Dont divide by zero!"
     }
     else{
         return a / b
@@ -114,6 +79,6 @@ function clearFields(){
 }
 
 function backspace(){
-     let val = document.querySelector("#textfield").value;
-      document.querySelector("#textfield").value = val.substr(0, val.length - 1)
+    let val = document.querySelector("#textfield").value;
+    document.querySelector("#textfield").value = val.substr(0, val.length - 1)
 }
